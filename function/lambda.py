694fa6ec947c803c10610b8b9cf44b0b41f80567
Luis Montenegro <luismontenegro.1995@gmail.com>
2024-02-08 11:56:32 -0300

    Update lambda.py

--- function/lambda.py
+++ function/lambda.py
@@ -2,7 +2,7 @@ import requests
 
 def lambda_handler(event, context):
   """
-  Función Lambda que se ejecuta para evitar la inactividad en un sitio web.
+  Función Lambda que se ejecuta cada minuto para evitar la inactividad en un sitio web.
   """
 
   # Obtiene la dirección del sitio web del evento
@@ -14,11 +14,15 @@ def lambda_handler(event, context):
   try:
     response = requests.get(url)
     if response.status_code == 200:
+      print(f"code:200, message:Solicitud exitosa al sitio: {url}")
       return {"code": 200, "message": f"Solicitud exitosa al sitio: {url}"}
     else:
+      print(f"code:{response.status_code}, message:Solicitud fallida: {url}")
       return {"code": response.status_code, "message": f"Solicitud fallida: {url}"}
   except requests.HTTPError as e:
     if e.response.status_code == 404:
+      print(f"code:400, message:Sitio web no encontrado: {url}")
       return {"code": 404, "message": f"Sitio web no encontrado: {url}"}
     else:
+      print(f"code:500, message:Error interno del servidor: {url}")
       return {"code": 500, "message": f"Error interno del servidor: {url}"}
